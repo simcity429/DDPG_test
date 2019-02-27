54645dec5ccc486f937ab646b20176e31796be3a
simcity429 <simcity429@naver.com>
2019-02-28 04:56:28 +0900

    second

--- DDPG.py
+++ DDPG.py
@@ -2,80 +2,135 @@ import gym
 import tensorflow as tf
 import numpy as np
 from random import shuffle
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.layers import Input
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Concatenate
 from sklearn.preprocessing import StandardScaler
-from scipy.stats import truncnorm
 
 def gaussian_noise_layer(input_layer, std):
     noise = tf.random_normal(shape=tf.shape(input_layer), mean=0.0, stddev=std, dtype=tf.float32)
     return input_layer + noise
 
+def assign_target(sess, main_network_param, target_network_param):
+    assign_op = []
+    for main_var, target_var in zip(main_network_param, target_network_param):
+        assign_op.append(target_var.assign(main_var.value()))
+    return sess.run(assign_op)
+
+class Actor:
+    def __init__(self, sess, state_dim, action_dim, learning_rate, tau, batch_size):
+        self.sess = sess
+        self.state_dim = state_dim
+        self.action_dim = action_dim
+        self.lr = learning_rate
+        self.tau = tau
+        self.batch_size = batch_size
+
+        self.input, out = self.create_actor('actor')
+        self.target_input, self.target_out = self.create_actor('target_actor')
+        self.main_network_param = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='actor')
+        self.target_network_param = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='target_actor')
+
+        self.action_gradient = tf.placeholder(dtype=tf.float32, shape=[None, self.action_dim], name='action_gradient')
+        tmp = tf.gradients(self.out, self.main_network_param, -self.action_gradient)
+        self.actor_gradient = list(map(lambda x: tf.div(x, self.batch_size), tmp))
+        self.optimize = tf.train.AdamOptimizer(self.lr).apply_gradients(zip(self.action_gradient, self.main_network_param))
+
+
+    def create_actor(self, name):
+        with tf.variable_scope(name):
+            state_input = tf.placeholder(dtype=tf.float32, shape=[None, self.state_dim], name='state_input')
+            out = tf.layers.dense(inputs=state_input, units=100, activation='elu')
+            out = tf.layers.batch_normalization(out)
+            out = tf.layers.dense(inputs=out, units=100, activation='elu')
+            out = tf.layers.dense(inputs=out, units=self.action_dim, activation='tanh', use_bias=False,
+                                  kernel_initializer=tf.initializers.random_uniform(minval=-0.003, maxval=0.003))
+        return state_input, out
+
+    def update_target(self):
+        copy_op = []
+        tau = self.tau
+        for main_var, target_var in zip(self.main_network_param, self.target_network_param):
+            copy_op.append(target_var.assign(tf.multiply(main_var.value(), tau) + tf.multiply(target_var.value(), 1 - tau)))
+        return self.sess.run(copy_op)
+
+    def train(self, input, action_gradient):
+        return self.sess.run(self.optimize, feed_dict={self.input: input, self.action_gradient: action_gradient})
+
+    def predict(self, input):
+        return self.sess.run(self.out, feed_dict={self.input: input})
+
+    def predict_target(self, input):
+        return self.sess.run(self.target_out, feed_dict={self.target_input: input})
+
+class Critic:
+    def __init__(self, sess, state_dim, action_dim, learning_rate, tau, batch_size):
+        self.sess = sess
+        self.state_dim = state_dim
+        self.action_dim = action_dim
+        self.lr = learning_rate
+        self.tau = tau
+        self.batch_size = batch_size
+
+        self.action_input, self.state_input, self.out = self.create_critic('critic')
+        self.target_action_input, self.target_state_input, self.target_out = self.create_critic('target_critic')
+
+        self.main_network_param = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='critic')
+        self.target_network_param = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='target_critic')
+
+        self.predicted_q = tf.placeholder(dtype=tf.float32, shape=[None, 1], name='predicted_q')
+
+        self.loss = tf.reduce_mean(tf.square(tf.subtract(self.predicted_q, self.out)))
+        self.optimize = tf.train.AdamOptimizer(self.lr).minimize(self.loss)
+
+        self.action_grads = tf.gradients(self.out, self.action_input)
+
+
+
+    def create_critic(self, name):
+        with tf.variable_scope(name):
+            action_input = tf.placeholder(dtype=tf.float32, shape=[None, self.action_dim], name='action_input')
+            state_input = tf.placeholder(dtype=tf.float32, shape=[None, self.state_dim], name='state_input')
+            process_state = tf.layers.dense(inputs=state_input, units=100, activation='elu')
+            process_state = tf.layers.batch_normalization(process_state)
+            process_state = tf.layers.dense(inputs=process_state, units=100, activation='elu')
+            process_action = tf.layers.dense(inputs=action_input, units=100, activation='elu')
+            process_action = tf.layers.batch_normalization(process_action)
+            out = tf.layers.dense(inputs=tf.add(process_state, process_action), units=50, activation='elu')
+            out = tf.layers.dense(inputs=out, units=1, kernel_initializer=tf.random_uniform_initializer(minval=-0.003, maxval=0.003))
+        return action_input, state_input, out
+
+    def update_target(self):
+        copy_op = []
+        tau = self.tau
+        for main_var, target_var in zip(self.main_network_param, self.target_network_param):
+            copy_op.append(target_var.assign(tf.multiply(main_var.value(), tau) + tf.multiply(target_var.value(), 1 - tau)))
+        return self.sess.run(copy_op)
+
+    def train(self, action_input, state_input, predicted_q):
+        return self.sess.run(self.optimize, feed_dict={self.action_input: action_input, self.state_input: state_input, self.predicted_q: predicted_q})
+
+    def action_gradient(self, action_input, state_input):
+        return self.sess.run(self.action_grads, feed_dict={self.action_input: action_input, self.state_input: state_input})
+
+    def predict(self, action_input, state_input):
+        return self.sess.run(self.out, feed_dict={self.action_input: action_input, self.state_input: state_input})
+
+    def predict_target(self, action_input, state_input):
+        return self.sess.run(self.target_out, feed_dict={self.target_action_input: action_input, self.target_state_input:state_input})
+
 class DDPG:
-    def __init__(self):
-        self.build_model()
-        self.build_target()
+    def __init__(self, state_dim, action_dim):
+        self.actor_lr = 0.001
+        self.critic_lr = 0.001
+        self.tau = 0.1
+        self.batch_size = 50
+        self.sess = tf.Session()
+        self.actor = Actor(self.sess, state_dim, action_dim, self.actor_lr, self.tau, self.batch_size)
+        self.critic = Critic(self.sess, state_dim, action_dim, self.critic_lr, self.tau, self.batch_size)
+        self.sess.run(tf.global_variables_initializer())
+        assign_target(self.sess, self.actor.main_network_param, self.actor.target_network_param)
+        assign_target(self.sess, self.critic.main_network_param, self.critic.target_network_param)
         self.MAX_LENGTH = 20000
         self.memory = []
 
-    def build_actor(self):
-        actor_state_input = tf.placeholder(dtype=tf.float32, shape=[None, 2])
-        actor_dense_1 = tf.layers.dense(inputs=actor_state_input, units=32, activation=tf.nn.elu)
-
-
-    def build_model(self):
-        state_input_c = tf.placeholder(tf.float32, shape=[None, 2], name="state_input_c")
-        reward_and_next_q = tf.placeholder(tf.float32, shape=[None, 1], name='reward_and_next_q')
-        action_input = tf.placeholder(tf.float32, shape=[None, 1], name='action_input')
-        with tf.variable_scope('critic'):
-            action_dense_1 = tf.layers.dense(inputs=action_input, units=32, activation=tf.nn.elu, name='dense_1')
-            state_dense = tf.layers.dense(inputs=state_input_c, units=32, activation=tf.nn.elu, name='dense_2')
-            concat = tf.concat([action_dense_1, state_dense], 1)
-            q_1 = tf.layers.dense(inputs= concat, units=1, name='dense_3')
-
-        with tf.variable_scope("actor"):
-            tmp = tf.layers.dense(inputs=state_dense, units=16, activation=tf.nn.elu, name='tmp')
-            action = tf.layers.dense(inputs=tmp, units=1, activation=tf.nn.tanh, name='action')
-
-        with tf.variable_scope('actor', reuse=True):
-            tmp = tf.layers.dense(inputs=state_dense, units=16, activation=tf.nn.elu, name='tmp')
-            debug = tf.layers.dense(inputs=tmp, units=1, name='action')
-
-        with tf.variable_scope('critic', reuse=True):
-            action_dense_2 = tf.layers.dense(inputs=action, units=32, activation=tf.nn.elu, name='dense_1', reuse=True)
-            concat = tf.concat([action_dense_2, state_dense], 1)
-            q_2 = tf.layers.dense(inputs=concat, units=1, name='dense_3', reuse=True)
-
-        loss_a = tf.reduce_mean(-q_2)
-        loss_c = tf.reduce_mean(tf.square(q_1 - reward_and_next_q))
-
-        self.debug = debug
-
-        actor_opt = tf.train.AdamOptimizer(learning_rate=0.001).minimize(loss_a,
-                                          var_list=tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='actor'))
-        critic_opt = tf.train.AdamOptimizer(learning_rate=0.001).minimize(loss_c,
-                                          var_list=tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='critic'))
-        print(tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='actor'))
-        print(tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='critic'))
-
-        self.loss_a = loss_a
-        self.loss_c = loss_c
-
-        self.action = action
-
-        self.action_input = action_input
-        self.state_input_c = state_input_c
-        self.reward_and_next_q = reward_and_next_q
-        self.q_1 = q_1
-        self.q_2 = q_2
-
-        self.actor_opt = actor_opt
-        self.critic_opt = critic_opt
-        self.session = tf.Session()
-        self.session.run(tf.global_variables_initializer())
 
     def get_action(self, state, ep):
         limit = 100
@@ -93,9 +148,8 @@ class DDPG:
         elif ep < 1000:
             ep /= 100
             c = 1 - (ep/10)
-        sess = self.session
         state = np.reshape(state, [1, -1])
-        action = sess.run(self.action, feed_dict={self.state_input_c: state})[0]
+        action = self.actor.predict_target(state)[0]
         print('real action: ', action)
         noise = np.random.normal(0, 1.5*c)
         action += noise
@@ -105,14 +159,6 @@ class DDPG:
             action[0] = -1
         return action
 
-    def get_q(self, state, action, flag):
-        sess = self.session
-        state = np.reshape(state, [1, -1])
-        if flag:
-            action = np.reshape(action, [1, -1])
-        else:
-            action = sess.run(self.action, feed_dict={self.state_input_c: state})
-        return sess.run(self.q_t, feed_dict={self.state_input_t: state, self.action_input_t: action})[0]
 
     def store_history(self, history):
         if len(self.memory) < self.MAX_LENGTH:
@@ -120,6 +166,7 @@ class DDPG:
         else:
             self.memory.pop(0)
             self.memory.append(history)
+        return
 
     def return_batch(self, batch_size):
         ret = []
@@ -129,8 +176,7 @@ class DDPG:
 
     def train(self, ep):
         limit = 100
-        sess = self.session
-        batch_sample = self.return_batch(150)
+        batch_sample = self.return_batch(self.batch_size)
         state_batch = []
         action_batch = []
         q_batch = []
@@ -143,37 +189,16 @@ class DDPG:
         q_batch = np.asarray(q_batch)
         if (ep>limit):
             print('action training')
-            sess.run(self.actor_opt, feed_dict={self.state_input_c: state_batch})
-        sess.run(self.critic_opt, feed_dict={self.state_input_c: state_batch, self.action_input:action_batch, self.reward_and_next_q:q_batch})
-        print(sess.run([self.loss_a, self.loss_c], feed_dict={self.state_input_c: state_batch, self.action_input:action_batch, self.reward_and_next_q:q_batch}))
-        print('debug: ', sess.run([self.debug], feed_dict={self.state_input_c: state_batch})[0][:5])
-
-
-    def build_target(self):
-        state_input_t = tf.placeholder(tf.float32, shape=[None, 2], name="state_input_t")
-        action_input_t = tf.placeholder(tf.float32, shape=[None, 1], name='action_input_t')
-        with tf.variable_scope('target'):
-            action_dense_1 = tf.layers.dense(inputs=action_input_t, units=32, activation=tf.nn.elu, name='dense_1')
-            state_dense = tf.layers.dense(inputs=state_input_t, units=32, activation=tf.nn.elu, name='dense_2')
-            concat = tf.concat([action_dense_1, state_dense], 1)
-            q_t = tf.layers.dense(inputs= concat, units=1, name='dense_3')
-
-        self.state_input_t = state_input_t
-        self.action_input_t = action_input_t
-        self.q_t = q_t
-        self.session.run(tf.global_variables_initializer())
-        self.update_target()
-
+            actor_grads = self.critic.action_gradient(action_batch, state_batch)
+            self.actor.train(state_batch, actor_grads)
+        self.critic.train(action_batch, state_batch, q_batch)
+        return
 
     def update_target(self):
-        tau = 0.5
-        copy_op = []
-        target_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='target')
-        critic_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='critic')
-        for critic_var, target_var in zip(critic_vars, target_vars):
-            copy_op.append(target_var.assign(tf.multiply(critic_var.value(), tau) + tf.multiply(target_var.value(), 1 - tau)))
+        self.actor.update_target()
+        self.critic.update_target()
+        return
 
-        self.session.run(copy_op)
 
 
 
@@ -183,6 +208,8 @@ if __name__ == "__main__":
     gamma = 0.99
     limit = 100
     G_dividor = 10
+    state_dim = 2
+    action_dim = 1
     resume = False
     env = gym.make('MountainCarContinuous-v0')
     observation_examples = np.array([env.observation_space.sample() for x in range(10000)])
@@ -191,7 +218,7 @@ if __name__ == "__main__":
     scaler_a = StandardScaler()
     scaler_s.fit(observation_examples)
     scaler_a.fit(action_examples)
-    agent = DDPG()
+    agent = DDPG(state_dim, action_dim)
     saver = tf.train.Saver(tf.global_variables())
     if resume:
         saver.restore(agent.session, './DDPG.ckpt')
